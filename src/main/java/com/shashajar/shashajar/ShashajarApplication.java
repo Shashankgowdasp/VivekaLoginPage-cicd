b855702b58a96e73c84621509603fb4d30f005ee
Shashank <shashankspgowda@gmail.com>
2024-09-30 16:06:46 +0530

    simple screen

---
+++ src/main/java/com/shashajar/shashajar/ShashajarApplication.java
@@ -0,0 +1,13 @@
+package com.shashajar.shashajar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShashajarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShashajarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashajar/shashajar/ShashajarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashajar.shashajar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShashajarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
